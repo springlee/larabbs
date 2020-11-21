a35fb3c66dca06bf56437331ec1057b9cbcc5db1
springlee <jslichun@sina.com>
2020-11-21 18:03:26 +0800

    用户注册

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -4,10 +4,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\VerificationCodeRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 use Overtrue\EasySms\EasySms;
-use Psy\Util\Str;
 
 class VerificationCodesController extends Controller
 {
@@ -19,12 +18,24 @@ class VerificationCodesController extends Controller
         $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
         try {
-            $result = $easySms->send($phone, [
-                'template' => config('easysms.gateways.aliyun.templates.register'),
-                'data' => [
-                    'code' => $code
-                ],
-            ]);
+            if (!app()->environment('production')) {
+                $code = '1234';
+            } else {
+                // 生成4位随机数，左侧补0
+                $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+                try {
+                    $result = $easySms->send($phone, [
+                        'template' => config('easysms.gateways.aliyun.templates.register'),
+                        'data' => [
+                            'code' => $code
+                        ],
+                    ]);
+                } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                    $message = $exception->getException('aliyun')->getMessage();
+                    abort(500, $message ?: '短信发送异常');
+                }
+            }
         } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
             $message = $exception->getException('aliyun')->getMessage();
             abort(500, $message ?: '短信发送异常');
